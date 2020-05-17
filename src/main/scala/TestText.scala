8b10af40e0aaa1b4097ad39362a2aa0732f7ac7d
nkohen <nadavk25@gmail.com>
2020-05-17 11:52:18 -0500

    Added range selection on double-click

--- src/main/scala/TestText.scala
+++ src/main/scala/TestText.scala
@@ -4,7 +4,7 @@ import scalafx.Includes._
 import scalafx.scene.Scene
 import scalafx.scene.control.{Alert, TextArea}
 import scalafx.scene.control.Alert.AlertType
-import scalafx.scene.input.{KeyCode, MouseEvent}
+import scalafx.scene.input.MouseEvent
 
 object TestText extends JFXApp {
   // Catch unhandled exceptions on FX Application thread
@@ -22,22 +22,31 @@ object TestText extends JFXApp {
       }
     )
 
-  private val textArea = new TextArea {
+  private val textArea: TextArea = new TextArea {
     editable = true
     wrapText = true
     promptText = "Type text here and ctrl+click on a word to duplicate it!"
 
+    def getCharIndex(event: MouseEvent): Int = {
+      val clickX = event.sceneX
+      val clickY = event.sceneY
+      skin().asInstanceOf[TextAreaSkin].getIndex(clickX, clickY).getCharIndex
+    }
+
+    def getWord(text: String, index: Int): (Int, Int) = {
+      val textClickedSuffix = text.drop(index).takeWhile(_.isLetterOrDigit)
+      val textClickedPrefix = text.take(index).reverse.takeWhile(_.isLetterOrDigit).reverse
+
+      val wordStart = index - textClickedPrefix.length
+      val wordLength = textClickedPrefix.length + textClickedSuffix.length
+
+      (wordStart, wordLength)
+    }
+
     onMouseClicked = { event =>
       if (event.controlDown) {
-        val clickX = event.sceneX
-        val clickY = event.sceneY
-        val index = skin().asInstanceOf[TextAreaSkin].getIndex(clickX, clickY).getCharIndex
-
-        val textClickedSuffix = text().drop(index).takeWhile(_.isLetterOrDigit)
-        val textClickedPrefix = text().take(index).reverse.takeWhile(_.isLetterOrDigit).reverse
-
-        val wordStart = index - textClickedPrefix.length
-        val wordLength = textClickedPrefix.length + textClickedSuffix.length
+        val index = getCharIndex(event)
+        val (wordStart, wordLength) = getWord(text(), index)
 
         if (wordLength > 0) {
           val (textBefore, textWithAndAfter) = text().splitAt(wordStart)
@@ -45,6 +54,14 @@ object TestText extends JFXApp {
 
           text = textBefore ++ s"($textClicked, $textClicked)" ++ textAfter
           this.positionCaret(text().length - textAfter.length)
+        }
+      } else if (event.clickCount == 2) {
+        val index = getCharIndex(event)
+        val (wordStart, wordLength) = getWord(text(), index)
+
+        if (wordLength > 0) {
+          this.positionCaret(wordStart)
+          this.extendSelection(wordStart + wordLength + 4)
         }
       }
     }
